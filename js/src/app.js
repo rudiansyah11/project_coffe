c2e70a7f1bff8e9037e64e02460b7e9e9a303a38
rudiansyah11 <rudiansyah.sukisno@gmail.com>
2024-06-18 19:25:27 +0700

    penambahan payment gateway midtrans

--- js/src/app.js
+++ js/src/app.js
@@ -64,6 +64,98 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+// form validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  let allFilled = true; // flag to check if all fields are filled
+
+  for (let i = 0; i < form.elements.length; i++) {
+    // Skip checking hidden inputs
+    if (
+      form.elements[i].type !== "hidden" &&
+      form.elements[i].type !== "submit"
+    ) {
+      if (form.elements[i].value.trim() === "") {
+        allFilled = false;
+        break;
+      }
+    }
+  }
+
+  if (allFilled) {
+    checkoutButton.disabled = false;
+    checkoutButton.classList.remove("disabled");
+  } else {
+    checkoutButton.disabled = true;
+    checkoutButton.classList.add("disabled");
+  }
+});
+
+// const checkoutButton = document.querySelector(".checkout-button");
+// checkoutButton.disabled = true;
+
+// const form = document.querySelector("#checkoutForm");
+
+// form.addEventListener("keyup", function () {
+//   for (let i = 0; i < form.elements.length; i++) {
+//     if (form.elements[i].value.length !== 0) {
+//       checkoutButton.classList.remove("disabled");
+//       checkoutButton.classList.add("disabled");
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   checkoutButton.disabled = false;
+//   checkoutButton.classList.remove("disabled");
+// });
+
+// submit form
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+
+  //   console.log(objData);
+  //   const message = formatMessage(objData);
+  //   window.open("http://wa.me/NO WA?text=" + encodeURIComponent(message));
+
+  // kirim dulu ke endpoint backend untuk mendapatkan tokennya.
+  try {
+    const response = await fetch("backend/order.php", {
+      method: "POST",
+      body: data,
+    });
+
+    const token = await response.text();
+    window.snap.pay(token);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// format pesan wa
+const formatMessage = (obj) => {
+  return `Data Customer
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No HP: ${obj.phone}
+
+    Pesanan:
+    ${JSON.parse(obj.items).map(
+      (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)} ) \n`
+    )}
+    TOTAL: ${rupiah(obj.total)}
+
+    Terima Kasih.`;
+};
+
 // konversi rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
